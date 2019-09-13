d3b65a1aadd3594f9d7aed842981904e57605f55
Piotr <piotr@fastmail.co.uk>
2019-09-13 18:15:33 +0100

    init

---
+++ size_change.py
@@ -0,0 +1,52 @@
+from PIL import Image
+import glob, os
+from ftplib import FTP
+
+
+def file_sizing():
+    size = 600, 600
+    for infile in glob.glob("../../portugal_2019/blog_pho/*.jpg"):
+        file, file_ext = os.path.splitext(infile)
+        path_name, file_name = os.path.split(file)
+        photo = Image.open(infile)
+        photo.thumbnail(size)
+        photo.save("../../SmallsLab/nowaZielews/files/uploads/blog/%s.jpg" %(file_name))
+        #Viewing EXIF data embedded in image
+        #exif_data = photo._getexif()
+        #print(exif_data)
+
+def ftp_conn():
+    server = os.environ.get('FTP_SERVER')
+    user = os.environ.get('FTP_USER')
+    password = os.environ.get('FTP_PASS')
+
+    ftp = FTP(server)
+    #ftp.set_debuglevel(1)
+    ftp.connect(server, 21)
+    ftp.login(user, password)
+    print(ftp.getwelcome())
+    ftp.cwd('domains/zielewski.com/public_html/files/uploads/blog/')
+
+    #file = open('../../portugal_2019/blog_pho/image.jpg','rb')       # file to send
+    srv_files = []
+    for items in ftp.nlst():
+        srv_files.append(items)
+
+    for pictures in glob.glob("../../SmallsLab/nowaZielews/files/uploads/blog/*.jpg"):
+        path_name, file_name = os.path.split(pictures)
+        file = open(pictures,'rb')       # file to send
+
+        if file_name in srv_files:
+            print('all up to date..')
+            pass
+        else:
+            print('uploading...')
+            ftp.storbinary("STOR " + file_name, file)   # send the file
+
+    #ftp.dir()
+    ftp.quit()
+
+
+
+file_sizing()
+ftp_conn()
